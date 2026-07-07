467def9de81ca1ac438173a4ebfd342e11d6b7d9
zazedd <leomendesantos@gmail.com>
2026-07-07 20:00:30 +0100

    update generator

--- generate-cask.rb
+++ generate-cask.rb
@@ -16,12 +16,16 @@ tap = Tap.fetch(tap_name)
 
 Cask::Cask.generating_hash!
 
-tap.cask_files.each do |path|
-  cask = Cask::CaskLoader.load(path)
-  name = cask.token
-  json = JSON.pretty_generate(cask.to_hash_with_variations)
+latest_macos = MacOSVersion.new(HOMEBREW_MACOS_NEWEST_SUPPORTED).to_sym
+default_arch = :intel  # NOTE: Change this to :arm if you want arm as default arch.
+Homebrew::SimulateSystem.with(os: latest_macos, arch: default_arch) do
+  tap.cask_files.each do |path|
+    cask = Cask::CaskLoader.load(path)
+    name = cask.token
+    json = JSON.pretty_generate(cask.to_hash_with_variations)
 
-  IO.write("_data/cask/#{name}.json", "#{json}\n")
-rescue
-  onoe "Error while generating data for '#{path.stem}'"
+    IO.write("_data/cask/#{name}.json", "#{json}\n")
+  rescue => e
+    warn "Error while generating data for '#{path.stem}': #{e.message}"
+  end
 end
